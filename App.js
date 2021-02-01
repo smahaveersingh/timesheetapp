5b9da10a48e8cb82eea7e4a7fb95bc815838110e
Mahaveer Singh <msingh@obelisk.com>
2021-02-01 09:22:42 +0000

    worked on calender

--- App.js
+++ App.js
@@ -1,147 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Constants from 'expo-constants';
 import * as SQLite from 'expo-sqlite';
 import Header from './components/Header';
+import CalendarPicker from 'react-native-calendar-picker';
 
-const db = SQLite.openDatabase('db.db');
-
-class Items extends React.Component {
-  state = {
-    items: null
-  };
-
-  componentDidMount() {
-    this.update();
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStartDate: null,
+    };
+    this.onDateChange = this.onDateChange.bind(this);
   }
 
-  render() {
-    const { done: doneHeading } = this.props;
-    const { items } = this.state;
-    const heading = doneHeading ? "Completed" : "Todo";
-
-    if (items === null || items.length === 0) {
-      return null;
-    }
-
-    return (
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionHeading}>{heading}</Text>
-        {items.map(({ id, done, value }) => (
-          <TouchableOpacity
-            key={id}
-            onPress={() => this.props.onPressItem && this.props.onPressItem(id)}
-            style={{
-              backgroundColor: done ? "#1c9963" : "#fff",
-              borderColor: "#000",
-              borderWidth: 1,
-              padding: 8
-            }}
-          >
-            <Text style={{ color: done ? "#fff" : "#000" }}>{value}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    );
-  }
-
-  update() {
-    db.transaction(tx => {
-      tx.executeSql(
-        `select * from items where done = ?;`,
-        [this.props.done ? 1 : 0],
-        (_, { rows: { _array } }) => this.setState({ items: _array })
-      );
-    });
-  }
-}
-
-export default class App extends React.Component {
-  state = {
-    text: null
-  };
-
-  componentDidMount() {
-    db.transaction(tx => {
-      tx.executeSql(
-        "create table if not exists items (id integer primary key not null, done int, value text);"
-      );
+  onDateChange(date) {
+    this.setState({
+      selectedStartDate: date,
     });
   }
 
   render() {
-    return (
+    const { selectedStartDate } = this.state;
+    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+   return (
       <View style={styles.container}>
         <Header title="Timesheets" />
         <View style={styles.flexRow}>
-          <TextInput
-            onChangeText={text => this.setState({ text })}
-            onSubmitEditing={() => {
-              this.add(this.state.text);
-              this.setState({ text: null });
-            }}
-            placeholder="what do you need to do?"
-            style={styles.input}
-            value={this.state.text}
-          />
+          <Text style={styles.heading}>Week Ending</Text>
         </View>
-        <ScrollView style={styles.listArea}>
-          <Items
-            done={false}
-            ref={todo => (this.todo = todo)}
-            onPressItem={id =>
-              db.transaction(
-                tx => {
-                  tx.executeSql(`update items set done = 1 where id = ?;`, [
-                    id
-                  ]);
-                },
-                null,
-                this.update
-              )
-            }
-          />
-          <Items
-            done={true}
-            ref={done => (this.done = done)}
-            onPressItem={id =>
-              db.transaction(
-                tx => {
-                  tx.executeSql(`delete from items where id = ?;`, [id]);
-                },
-                null,
-                this.update
-              )
-            }
-          />
-        </ScrollView>
+        <View>
+          <Text>{ startDate }</Text>
+        </View>
+        <CalendarPicker
+          onDateChange={this.onDateChange}
+        />
+        
       </View>
-      
     );
   }
-
-  add(text) {
-    // is text empty?
-    if (text === null || text === "") {
-      return false;
-    }
-
-    db.transaction(
-      tx => {
-        tx.executeSql("insert into items (done, value) values (0, ?)", [text]);
-        tx.executeSql("select * from items", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-        );
-      },
-      null,
-      this.update
-    );
-  }
-
-  update = () => {
-    this.todo && this.todo.update();
-    this.done && this.done.update();
-  };
 }
 
 const styles = StyleSheet.create({
@@ -153,7 +50,10 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 20,
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    color: 'black',
+    paddingTop: 26,
+    paddingLeft: 10,
   },
   flexRow: {
     flexDirection: "row"
@@ -164,6 +64,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     flex: 1,
     height: 48,
+    width: 68,
     margin: 16,
     padding: 8
   },
@@ -179,5 +80,20 @@ const styles = StyleSheet.create({
   sectionHeading: {
     fontSize: 18,
     marginBottom: 8
+  },
+  calender: {
+    paddingTop: 96,
+    paddingLeft: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginLeft: -240
+  },
+  showdate: {
+    fontSize: 15,
+    color: 'black',
+    paddingTop: 56,
+    paddingLeft: 10,
+    marginLeft: -26
   }
 });
